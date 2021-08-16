9e260cd264b87e2e61f48bccb1d780ebffee87f2
姜佳春 <jjccoke@gmail.com>
2021-08-16 16:07:27 +0800

    Spring Boot使用JNDI连接MySql

---
+++ src/main/java/com/jjc/SpringBootJndiApplication.java
@@ -0,0 +1,13 @@
+package com.jjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJndiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJndiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jjc/TestJndi.java
@@ -0,0 +1,27 @@
+package com.jjc;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestJndi {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public TestJndi(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        test();
+    }
+    
+    public void test() {
+        List<Map<String, Object>> users = jdbcTemplate.queryForList("select * from t_user");
+        users.forEach(user -> {
+            System.out.println(user.get("name"));
+        });
+    }
+}
